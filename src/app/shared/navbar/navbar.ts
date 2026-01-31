8b8259ddba6f75109f337ae62e1ff84fbac8822b
Diwakar_Rai <rdiwakark23@gmail.com>
2026-01-31 12:35:56 +0530

    navbar created

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/auth/auth.service';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  // public: because we want to use this service directly in the template
+  public authService = inject(AuthService);
+  private router = inject(Router);
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
